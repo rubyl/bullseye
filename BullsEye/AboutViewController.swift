71341068bc520a419c3477eeb81efb13152f0e71
Ruby Lim <ruby.b.lim@gmail.com>
2018-11-20 19:10:46 -0800

    Initial Commit

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Ruby Lim on 11/16/18.
+//  Copyright © 2018 Ruby Lim. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html") {
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+        
+    }
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ BullsEye/AuthorViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AuthorViewController.swift
+//  BullsEye
+//
+//  Created by Ruby Lim on 11/16/18.
+//  Copyright © 2018 Ruby Lim. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
